fbc3be808e5e94a16add2a137d2fb7b68f36c342
Nat <98876033+lslxbrnv7@users.noreply.github.com>
2023-08-10 13:30:50 +0800

    - updated migrations

--- database/migrations/2023_08_09_150448_create_locations_table.php
+++ database/migrations/2023_08_09_150448_create_locations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->float('longitude');
-            $table->string('latitude');
+            $table->float('latitude');
         });
     }
 
